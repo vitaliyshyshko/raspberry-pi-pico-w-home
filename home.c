9e9871ec62f5b30f6124cb16be12b6f6cd82c03b
Vitalii Shyshko <ukraine.linux@gmail.com>
2024-04-25 03:50:09 +0300

    Added project structure

---
+++ home.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+int main() {
+    stdio_init_all();
+
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
+        printf("failed to initialise\n");
+        return 1;
+    }
+    printf("initialised\n");
+
+    cyw43_arch_enable_sta_mode();
+
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+        printf("failed to connect\n");
+        return 1;
+    }
+    printf("connected\n");
+}
